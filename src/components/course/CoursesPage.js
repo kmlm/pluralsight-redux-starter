2e85f31acba8efb3806f2084227eac745b6a37af
kmlm <kevin.mcaleese@libertymutual.com>
2017-12-15 16:20:55 -0500

    Refactored and added thunk
    Thunk added to configureStore through middleware
    
    Took out form and functions from CreatePages

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,28 +6,11 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
 
   constructor (props, context) {
-    super(props, context)
-
-    this.state = {
-      course: {title: ''}
+    super(props, context);
     }
-
-    this.onTitleChange = this.onTitleChange.bind(this)
-    this.onClickSave = this.onClickSave.bind(this)
   }
 
-  onTitleChange (event) {
-    const course = this.state.course
-    course.title = event.target.value
-    this.setState({ course: course })
-    console.log(this.state.course.title)
-  }
-
-  onClickSave () {
-    this.props.createCourse(this.state.course)
-  }
-
-  courseRow (course, index) {
+  courseRow(course, index) {
     return <div key={index}>
       {course.title}
         </div>
@@ -39,10 +22,6 @@ class CoursesPage extends React.Component {
       <div>
         <h1> Courses </h1>
         {this.props.courses.map(this.courseRow)}
-        <h2> Add Course</h2>
-          <input type="text" onChange={this.onTitleChange}
-                 value={this.state.course.title} />
-          <input type="submit" value="save " onClick={this.onClickSave}/>
       </div>
     )
   }
@@ -50,12 +29,12 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired
+  // createCourse: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
   return {
-    courses: state.courses
+    // courses: state.courses
   }
 }
 
@@ -68,3 +47,23 @@ function mapDispatchToProps(dispatch) {
 export default connect(mapStateToProps, mapDispatchToProps) (CoursesPage);
 
 
+
+
+//  <h2> Add Course</h2>
+{/*<input type="text" onChange={this.onTitleChange}*/}
+       {/*value={this.state.course.title} />*/}
+{/*<input type="submit" value="save " onClick={this.onClickSave}/>*/}
+
+{/*onTitleChange (event) {*/}
+  {/*const course = this.state.course*/}
+  {/*course.title = event.target.value*/}
+  {/*this.setState({ course: course })*/}
+  {/*console.log(this.state.course.title)*/}
+{/*}*/}
+
+{/*onClickSave () {*/}
+  {/*this.props.createCourse(this.state.course)*/}
+{/*}*/}
+
+{/*this.onTitleChange = this.onTitleChange.bind(this)*/}
+{/*this.onClickSave = this.onClickSave.bind(this)*/}
