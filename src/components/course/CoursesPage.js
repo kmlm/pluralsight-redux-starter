ec489b672e72255dd09a43741ca13e3b4b7f3f23
kmlm <kevin.mcaleese@libertymutual.com>
2017-12-15 14:27:50 -0500

    Added input for adding new course
    onChange and onClick bound in constructor

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,9 +1,43 @@
 import React, {PropTypes} from 'react';
 
-const CoursesPage = () => {
-  return (
-  <p> test</p>
-  )
+class CoursesPage extends React.Component {
+
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      course: {title: ''}
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange (event) {
+    const course = this.state.course
+    course.title = event.target.value
+    this.setState({ course: course })
+    console.log(this.state.course.title)
+  }
+
+  onClickSave (event) {
+    alert(`Saving`)
+  }
+
+  render () {
+    return (
+      <div>
+        <h1> Courses </h1>
+        <h2> Add Course</h2>
+          <input type="text" onChange={this.onTitleChange}
+                 value={this.state.course.title} />
+
+          <input type="submit" value="save " onClick={this.onClickSave}/>
+      </div>
+    )
+  }
 }
 
 export default CoursesPage;
+
+
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 class HomePage extends React.Component {
   render () {
     return (
-      <div className='jumbotron'>
+      <div className="jumbotron">
         <h1> PluralSight Admin </h1>
         <p> React, Redux and React Router in ES6 for ultra-reponsive web apps.</p>
         <Link to="about" className="btn btn-primary btn-lg"> Learn More </Link>
