a106ccebcf103f7283694c641f9ff7477911ffc4
kmlm <kevin.mcaleese@libertymutual.com>
2017-12-15 11:09:59 -0500

    Inital Commit
    Homepage and about page set up
    Initial Routes Created

---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class About extends React.Component {
+  render () {
+    return (
+      <div className='jumbotron'>
+        <h1>About </h1>
+        <p> About page here</p>
+      </div>
+    )
+}
+}
+
+export default About;
